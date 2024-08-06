f3c7ffb5bf84f44477ecd7effe36a6f069ea9647
Pascal7601 <levispascal63@gmail.com>
2024-08-06 13:33:58 +0000

    await function

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+"""
+file about the wait_random function
+"""
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    a function that uses random method 
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+print(asyncio.run(wait_random()))
+print(asyncio.run(wait_random(5)))
+print(asyncio.run(wait_random(15)))
+
