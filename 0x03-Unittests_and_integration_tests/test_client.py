d90b67ac57ea118fa872455a5d18068f6084a7fa
Pascal7601 <levispascal63@gmail.com>
2025-09-20 15:34:48 +0300

    tested _public_repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 import unittest
 from parameterized import parameterized
 import client
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -25,3 +25,19 @@ class TestGithubOrgClient(unittest.TestCase):
         result = org.org
         self.assertEqual(result, expected)
         mock_get.assert_called_once_with(f"{api}/{org_name}")
+
+    def test_public_repos_url(self):
+        """
+        tests the public_repos_url whethger it returns the
+        required result while mocking the org method
+        """
+        expected = "https://api.github.com/orgs/google"
+        payload = {"repos_url": expected}
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+                 ) as mock_obj:
+            client_obj = client.GithubOrgClient("google")
+            mock_obj.return_value = payload
+            test_result = client_obj._public_repos_url
+            self.assertEqual(payload["repos_url"], test_result)
