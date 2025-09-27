3a661eaa0190b03287621896470059989057706a
Pascal7601 <levispascal63@gmail.com>
2025-09-27 10:47:59 +0300

    modified the importation of permissions from drf

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,7 +1,7 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-from .models import User
+from rest_framework import permissions
 
-class IsMessageOwnerOrReadOnly(BasePermission):
+
+class IsMessageOwnerOrReadOnly(permissions.BasePermission):
     """
     allows the one who sent the mesage to edit or delete it while others can only read
     """
@@ -11,17 +11,17 @@ class IsMessageOwnerOrReadOnly(BasePermission):
         if request.user not in obj.conversation.participants.all():
             return False
         
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return True
         
         return obj.sender == request.user
     
-class IsPartOfConversation(BasePermission):
+class IsPartOfConversation(permissions.BasePermission):
     """
     allows users who are the partcipants to view the message
     """
     
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if request.method in permissions.SAFE_METHODS:
             return request.user in obj.participants.all()
         return request.user in obj.participants.all()
