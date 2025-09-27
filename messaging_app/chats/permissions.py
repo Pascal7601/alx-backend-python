f016adecb5a8c07cfdfd2eea1f6fc5dd82c59707
Pascal7601 <levispascal63@gmail.com>
2025-09-27 11:30:34 +0300

    modified my viewsets to implement the permissions i created

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
 
+class IsAuthenticatedUser(permissions.BasePermission):
+    """checks if the user is authenticated"""
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
 
 class IsMessageOwnerOrReadOnly(permissions.BasePermission):
     """
     allows the one who sent the mesage to edit or delete it while others can only read
-    """
+    """        
 
     def has_object_permission(self, request, view, obj):
         # check first whether the user is part of the conversation
@@ -14,7 +18,8 @@ class IsMessageOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         
-        return obj.sender == request.user
+        if request.method in ["PATCH", "PUT", "DELETE"]:
+            return obj.sender == request.user
     
 class IsPartOfConversation(permissions.BasePermission):
     """
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, status
 from .models import Conversation, Message
 from .serializers import MessageSerializer, CoversationSerializer
-from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthenticatedUser, IsMessageOwnerOrReadOnly, IsPartOfConversation
 from rest_framework.response import Response
 
 
@@ -10,25 +10,52 @@ from rest_framework.response import Response
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
     queryset = Message.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedUser, IsMessageOwnerOrReadOnly, IsPartOfConversation]
+
+    def get_queryset(self):
+        # only return conversations that the user is part of
+        return Message.objects.filter(conversation__participants=self.request.user)
 
     def create(self, request, *args, **kwargs):
         """Send a new message to an existing conversation"""
-        data = request.data.copy()
-        data["sender"] = request.user.id
-        serializer = self.get_serializer(data=data)
+        conversation_id = request.data.get("conversation")
+        if not conversation_id:
+            return Response({"detail: conversation_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            conversation = Conversation.objects.get(pk=conversation_id)
+        except Conversation.DoesNotExist:
+            return Response({"detail: conversation not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        if request.user not in conversation.participants.all():
+            return Response({"detail: not a participant of this convo"}, status=status.HTTP_403_FORBIDDEN)
+        
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save(sender=request.user, conversation=conversation)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     serializer_class = CoversationSerializer
     queryset = Conversation.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticatedUser, IsPartOfConversation]
+
+    def get_queryset(self):
+        # return only the convo that the user is part of
+        return Conversation.objects.filter(participants__all=self.request.user)
 
     def create(self, request, *args, **kwargs):
         """Create a new conversation with participants"""
         participants_ids = request.data.get("participants", [])
+        if not participants_ids:
+            return Response({"detail": "At least one participant is required."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        participants_ids = [int(pid) for pid in participants_ids]
+
+        if request.user.user_id not in participants_ids:
+            participants_ids.append(request.user.user_id)
+
         conversation = Conversation.objects.create()
         conversation.participants.set(participants_ids)
         conversation.save()
