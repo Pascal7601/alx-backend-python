cf5b5c03506e2d5116c6405690d16e12d64318f1
Pascal7601 <levispascal63@gmail.com>
2025-10-12 21:28:02 +0300

    docker compose

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "NAME": "messagedb",
         "USER": "pascal",
         "PASSWORD": "pascal",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "3306",
     }
 }
