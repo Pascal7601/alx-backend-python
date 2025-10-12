fc45b751cc6564a98f4a5210fef7bedbd4b1df7f
Pascal7601 <levispascal63@gmail.com>
2025-10-12 19:29:22 +0300

    configured db and the python API in the docker-compose file

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "messagedb",
+        "USER": "pascal",
+        "PASSWORD": "pascal",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
